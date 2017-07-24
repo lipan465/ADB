14e9d223e7ec9d360a7356491c37816004f2a239
li <lipan465@qq.com>
2017-07-24 18:35:57 +0800

    add adb

---
+++ adb/stdafx.h
@@ -0,0 +1,17 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+
+
+#define ZLIB_WINAPI
+// TODO: 在此处引用程序需要的其他头文件
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "AdbWinUsbApi.lib")
+#pragma comment(lib, "AdbWinApi.lib")
+#pragma comment(lib, "zlibstat.lib")
